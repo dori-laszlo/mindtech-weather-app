7e82d7381ba0edc0ab0753394d77a5ccae980bdf
Dóra László <doralaszlo@Doras-MacBook-Pro.local>
2018-08-21 21:48:46 +0200

    moved getDataFromApi function from App.js to a separated file and it got error handling. handleSubmit method can set the state with the weather data.

--- src/App.js
+++ src/App.js
@@ -2,30 +2,25 @@ import React, { Component } from 'react';
 import Logo from './components/Logo';
 import Title from './components/Title';
 import Select from './components/Select';
+import getDataFromApi from './api/getDataFromApi';
 import WeatherTile from './components/WeatherTile';
 import './app.css';
 
-const API_KEY = '94405e6cf2cc96fb5e6cd10c101446c7';
 
 class App extends Component {
   // The state object will contain the ID of the selected city, to forward it later
   // to the url in the API call as a city id to identify the selected city, which is null by default.
   state = {
     selectedCity: null,
-  };
-
-  // This method calls the API and get data from the city which has the ID what this method
-  // get from the handleSubmit method as parameter.
-  getDataFromApi = id => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${API_KEY}&units=metric`,
-    )
-      .then(res => res.json())
-      .then(json => console.log(json));
+    city: null,
+    country: null,
+    temperature: null,
+    description: null,
+    humidity: null,
   };
 
   // This method handles the event when select option has changed in the form.
-  // It takes the selected option's value which is the "id" key of each city from the city-list. 
+  // It takes the selected option's value which is the "id" key of each city from the city-list.
   // Then it set the selectedCity in the state object to this unique ID.
   handleChange = e => {
     const cityId = e.target.value;
@@ -36,11 +31,21 @@ class App extends Component {
   };
 
   // This method is triggered when the form has been submitted by the submit button.
-  // It calls the getDataFromApi method with the selected city's ID as a property.
+  // It calls the getDataFromApi function with the selected city's ID as a property.
+  // It sets the state object to the data get from the getDataFromApi function.
   handleSubmit = e => {
     e.preventDefault();
-    this.getDataFromApi(this.state.selectedCity);
-  }
+    getDataFromApi(this.state.selectedCity).then(data =>
+      this.setState({
+        city: data.name,
+        country: data.sys.country,
+        temperature: data.main.temp,
+        description: data.weather[0].description,
+        humidity: data.main.humidity,
+      }),
+    );
+    console.log(this.state.city + this.state.temperature);
+  };
 
   render() {
     return (
---
+++ src/api/getDataFromApi.js
@@ -0,0 +1,27 @@
+const API_KEY = '94405e6cf2cc96fb5e6cd10c101446c7';
+
+// This function calls the API and get data from the city which has the ID what this function
+// gets from the handleSubmit method (in App.js) as parameter.
+function getDataFromApi(id) {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${API_KEY}&units=metric`,
+  )
+    .then(response => {
+      if (response.ok) {
+        return Promise.resolve(response);
+      } else {
+        return Promise.reject(new Error('Failed to load'));
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return data;
+    })
+    .catch(error => {
+      console.log(`Error: ${error.message}`);
+      throw error;
+    });
+}
+
+export default getDataFromApi;
